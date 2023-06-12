f406440c609f226438931ae5fed43b7ebc2ff4c1
Hexiro <42787085+Hexiro@users.noreply.github.com>
2023-06-12 03:43:53 -0400

    base client

--- lawg/base/client.py
+++ lawg/base/client.py
@@ -1,18 +1,20 @@
 from abc import ABC, abstractmethod
 
-from marshmallow import Schema
 
 from lawg.base.log import BaseLog
 from lawg.base.rest import BaseRest
-from lawg.schemas import ProjectNameSchema, ProjectNamespaceSchema
+from lawg.base.project import BaseProject
+from lawg.base.room import BaseRoom
 
 
 class BaseClient(ABC):
-    rest: BaseRest
-
     def __init__(self, token: str) -> None:
         super().__init__()
         self.token: str = token
+        self.rest: BaseRest
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} token={self.token!r}>"
 
     @abstractmethod
     def create(
@@ -20,14 +22,70 @@ class BaseClient(ABC):
         name: str,
         namespace: str,
     ) -> BaseLog:
-        ...
-        # schema = CreateProjectSchema()
-        # schema.validate({"name": name, "namespace": namespace})
+        """
+        Create a log.
 
+        Args:
+            name (str): name of log.
+            namespace (str): namespace of log.
+        """
+
+    @abstractmethod
     def edit(
         self,
-        id: str,
         name: str,
         namespace: str,
     ) -> BaseLog:
-        ...
+        """
+        Edit a log.
+
+        Args:
+            name (str): name of log, what is being changed.
+            namespace (str): namespace of log.
+        """
+
+    @abstractmethod
+    def delete(
+        self,
+        namespace: str,
+    ) -> BaseLog:
+        """
+        Delete a log.
+
+        Args:
+            namespace (str): id of event.
+        """
+
+    @abstractmethod
+    def fetch(
+        self,
+        namespace: str,
+    ) -> BaseLog:
+        """
+        Fetch a log.
+
+        Args:
+            namespace (str): namespace of log.
+        """
+
+    patch = edit
+    get = fetch
+
+    @abstractmethod
+    def project(self, namespace: str) -> BaseProject:
+        """
+        Get a project.
+
+        Args:
+            namespace (str): namespace of project.
+        """
+
+    @abstractmethod
+    def room(self, namespace: str, room_name: str) -> BaseRoom:
+        """
+        Get a room.
+
+        Args:
+            namespace (str): namespace of project.
+            room_name (str): name of room.
+        """
